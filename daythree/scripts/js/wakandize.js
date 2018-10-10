319a17be2a8675b558cb2caed1599b5b1af9cb13
Udochi Okeke <udochio@hotmail.com>
2018-10-09 19:23:53 -0700

    Add wakandize.js via upload

---
+++ daythree/scripts/js/wakandize.js
@@ -0,0 +1,10 @@
+
+submit.addEventListener("click", wakandize);
+
+function wakandize(){
+
+var wname = "T\'Ch"+document.getElementById("name").value;
+
+document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"../styles/wakandize.css\"><h1>Your Wakanda name is:</h1><br><h2>"+wname+"</h2>");
+
+}
